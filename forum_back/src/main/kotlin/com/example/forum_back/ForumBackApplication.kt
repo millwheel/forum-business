912cc0d8ebdd0146261e70768cb4f90f944c5501
season <sionwer4@naver.com>
2024-11-10 21:33:42 +0900

    feat: Create forum back project

---
+++ forum_back/src/main/kotlin/com/example/forum_back/ForumBackApplication.kt
@@ -0,0 +1,11 @@
+package com.example.forum_back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForumBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<ForumBackApplication>(*args)
+}
---
+++ forum_back/src/test/kotlin/com/example/forum_back/ForumBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.forum_back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumBackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
