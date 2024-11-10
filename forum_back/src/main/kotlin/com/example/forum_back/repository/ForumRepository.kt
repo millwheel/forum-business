4216fbf71f57384a199cfdf3780c88b4567e4878
season <sionwer4@naver.com>
2024-11-10 22:15:22 +0900

    feat: Add query method to forum repository

--- forum_back/src/main/kotlin/com/example/forum_back/repository/ForumRepository.kt
+++ forum_back/src/main/kotlin/com/example/forum_back/repository/ForumRepository.kt
@@ -4,4 +4,8 @@ import com.example.forum_back.entity.Forum
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ForumRepository : JpaRepository<Forum, Long> {
+
+    fun findByTitle(title: String): Forum?
+    fun findByAuthor(author: String): Forum?
+
 }
