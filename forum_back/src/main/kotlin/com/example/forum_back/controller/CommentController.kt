a872ffe3884d6caa5605dd43a097be9ade6d8114
season <sionwer4@naver.com>
2024-12-08 23:08:32 +0900

    feat: Create comment controller

---
+++ forum_back/src/main/kotlin/com/example/forum_back/controller/CommentController.kt
@@ -0,0 +1,44 @@
+package com.example.forum_back.controller
+
+import com.example.forum_back.dto.comment.CommentCreateRequest
+import com.example.forum_back.dto.comment.CommentResponse
+import com.example.forum_back.dto.comment.CommentUpdateRequest
+import com.example.forum_back.service.CommentService
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/comments")
+class CommentController(
+    private val commentService: CommentService
+) {
+
+    @GetMapping("/forum/{forumId}")
+    fun getCommentsByForumId(@PathVariable forumId: Long): List<CommentResponse> {
+        val comments = commentService.getCommentsByForumId(forumId)
+        return comments.map { CommentResponse.of(it) }
+    }
+
+    @PostMapping("/forum/{forumId}")
+    fun createComment(
+        @PathVariable forumId: Long,
+        @Valid @RequestBody commentCreateRequest: CommentCreateRequest
+    ): CommentResponse {
+        val createdComment = commentService.createComment(forumId, commentCreateRequest)
+        return CommentResponse.of(createdComment)
+    }
+
+    @PutMapping("/{commentId}")
+    fun updateComment(
+        @PathVariable commentId: Long,
+        @Valid @RequestBody commentUpdateRequest: CommentUpdateRequest
+    ): CommentResponse {
+        val updatedComment = commentService.updateComment(commentId, commentUpdateRequest)
+        return CommentResponse.of(updatedComment)
+    }
+
+    @DeleteMapping("/{commentId}")
+    fun deleteComment(@PathVariable commentId: Long) {
+        commentService.deleteComment(commentId)
+    }
+}
---
+++ forum_back/src/main/kotlin/com/example/forum_back/dto/comment/CommentResponse.kt
@@ -0,0 +1,19 @@
+package com.example.forum_back.dto.comment
+
+import com.example.forum_back.entity.Comment
+
+data class CommentResponse (
+    val id: Long,
+    val description: String,
+    val author: String
+) {
+    companion object {
+        fun of(comment: Comment): CommentResponse {
+            return CommentResponse(
+                id = comment.id!!,
+                description = comment.description,
+                author = comment.author
+            )
+        }
+    }
+}
