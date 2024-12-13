174d14540871fde74e535ef08e1c176363fe6d73
season <sionwer4@naver.com>
2024-12-13 18:59:58 +0900

    feat: Add not found page'

--- forum-front/src/app/page.tsx
+++ forum-front/src/app/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <h1>Hello!</h1>
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function HomePage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = document.cookie.includes("access_token");
+    if (token) {
+      router.push("/forums");
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome to Forum</h1>
+    </div>
+  );
 }
