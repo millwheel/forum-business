e6106a6f6cdfe27e26504de24f8ec622254aa34e
season <sionwer4@naver.com>
2024-12-14 17:48:22 +0900

    feat: openai api sender kotlin version

---
+++ forum_back/src/main/kotlin/com/example/forum_back/sender/OpenaiApiSender.kt
@@ -0,0 +1,38 @@
+package com.example.forum_back.sender
+
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestClient
+
+@Component
+class OpenaiApiSender(
+    private val openaiProperties: OpenaiProperties
+) {
+
+    private var restClient = RestClient.builder()
+        .baseUrl("https://api.openai.com/v1/chat/completions")
+        .defaultHeader("Authorization", "Bearer ${openaiProperties.key}")
+        .defaultHeader("Content-Type", "application/json")
+        .build()
+
+    fun sendTranslateRequest(text: String) {
+        val result = restClient.post()
+            .body(buildRequestBody(text))
+            .retrieve()
+            .onStatus({ status -> status.is4xxClientError }) { request, response ->
+                throw RuntimeException("Error with code: ${response.statusCode}, with header: ${response.headers}")
+            }
+            .body(String::class.java)
+
+        println(result)
+    }
+
+    private fun buildRequestBody(text: String): ChatCompletionRequest {
+        return ChatCompletionRequest(
+            model = "gpt-4o-mini",
+            messages = listOf(
+                ChatMessage(role = "system", content = "You are a helpful assistant."),
+                ChatMessage(role = "user", content = text)
+            )
+        )
+    }
+}
