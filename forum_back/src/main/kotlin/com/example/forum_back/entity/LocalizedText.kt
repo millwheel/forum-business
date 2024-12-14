d69bf4df7d046fcbd1383e2b5ed051e2d0d5322d
season <sionwer4@naver.com>
2024-12-14 12:39:32 +0900

    feat: Create localizedText

--- forum_back/src/main/kotlin/com/example/forum_back/entity/LocalizedText.kt
+++ forum_back/src/main/kotlin/com/example/forum_back/entity/LocalizedText.kt
@@ -1,6 +1,11 @@
 package com.example.forum_back.entity
 
-class LocalizedText (
+import com.example.forum_back.enums.Language
+import jakarta.persistence.MappedSuperclass
 
-) {
+@MappedSuperclass
+abstract class LocalizedText (
+    language: Language
+) : BaseTime() {
+
 }
